07963869046f01d3c2493c551533dd70462c060c
Chris Nunez <cenunez47@gmail.com>
2017-09-18 00:29:25 +0000

    automatically backed up by learn

--- times.rb
+++ times.rb
@@ -4,9 +4,9 @@ def using_times
   end
 end
 
-tiny_bananas = 100
-7.times do
-  puts "Taking away seven tiny bananas"
-  tiny_bananas = tiny_bananas - 7
-  puts "Now if you've only got #{tiny_bananas} left!"
-end
+# tiny_bananas = 100
+# 9.times do
+#   puts "Taking away seven tiny bananas"
+#   tiny_bananas = tiny_bananas - 9
+#   puts "Now if you've only got #{tiny_bananas} left!"
+# end
